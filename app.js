c7b5bc0da5e6cd36c045f0c4cb7ceb59edbeeb01
aladamm78 <aladamm78@gmail.com>
2024-10-16 12:07:57 -0400

    step 3

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-/** BizTime express application. */
-
-
+// app.js
 const express = require("express");
-
 const app = express();
-const ExpressError = require("./expressError")
+const ExpressError = require("./expressError");
+const companiesRoutes = require("./routes/companies");
+const invoicesRoutes = require("./routes/invoices");
 
-app.use(express.json());
+app.use(express.json()); // Middleware to parse JSON
 
+app.use("/companies", companiesRoutes);
+app.use("/invoices", invoicesRoutes); // Use the invoices routes
 
-/** 404 handler */
-
-app.use(function(req, res, next) {
+// 404 handler
+app.use((req, res, next) => {
   const err = new ExpressError("Not Found", 404);
   return next(err);
 });
 
-/** general error handler */
-
+// General error handler
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
-
   return res.json({
     error: err,
-    message: err.message
+    message: err.message,
   });
 });
-
 
 module.exports = app;
--- db.js
+++ db.js
@@ -1,2 +1,10 @@
-/** Database setup for BizTime. */
+// db.js
+const { Client } = require("pg");
 
+const client = new Client({
+  connectionString: "postgresql://aladamm78:ali011380@localhost/biztime"
+});
+
+client.connect();
+
+module.exports = client;
